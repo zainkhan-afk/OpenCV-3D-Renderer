ea96d5e8b69f43a808baa96a1cc3a9a6ff0d980e
zainkhan-afk <zain.9496@gmail.com>
2022-08-12 18:00:03 +0500

    Adjusted camera parameters

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ scene = Scene(width = scene_width, height = scene_height)
 cube1 = Cube(x = 3, y = 2, z = -2)
 cube2 = Cube(x = -3, y = 3, z = -1)
 cube3 = Cube(x = 0, y = 0, z = 1)
-camera = Camera(x = 0, y = 0, z = 7, cx = scene_width//2, cy = scene_height//2)
+camera = Camera(x = 0, y = 0, z = 20, cx = scene_width//2, cy = scene_height//2, fx = 1000, fy = 1000)
 
 scene.add_camera(camera)
 scene.add_object(cube1)
@@ -19,10 +19,25 @@ scene.add_object(cube3)
 
 
 ang = 0
+camera_x = 0
+camera_y = 0
+camera_z = 7
 while True:
 	k = scene.render_scene()
 	camera.rotate(0, ang, 0)
-	# cube1.rotate(0, ang, 0)
+	# camera.translate(camera_x, camera_y, camera_z)
 	ang += 0.1
 	if k == ord("q"):
 		break
+
+	# if k == ord("w"):
+	# 	camera_z -= 0.5
+
+	# if k == ord("s"):
+	# 	camera_z += 0.5
+
+	# if k == ord("a"):
+	# 	camera_x -= 0.5
+
+	# if k == ord("d"):
+	# 	camera_x += 0.5
